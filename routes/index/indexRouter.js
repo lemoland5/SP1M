68ec3c64d52e1921a01c4f1bf333e9852b495b19
lemoland5 <lemo.cam.channel@gmail.com>
2024-11-18 10:13:37 +0100

    fix legacy imports

--- routes/index/indexRouter.js
+++ routes/index/indexRouter.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const db_utils = require("../../lib/db_utilities");
-const session_utils = require("../../lib/session_utilities")
 
 router.get("/", async (req, res) => {
     // const { db, client } = await db_utils.get_db();
