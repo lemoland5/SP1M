9ae40de8c4149e84c3c54b4dc2bfe2948cc7da44
lemoland5 <lemo.cam.channel@gmail.com>
2024-11-24 21:47:26 +0100

    Projekty done

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ app.use("/",express.static("./node_modules/bootstrap/dist/"));
 //define routes:
 app.use("/", mainRouter);
 
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -9,7 +9,9 @@ const docsRouter = require('./docs/docsRouter')
 const kadraRouter = require('./kadra/kadraRouter')
 const wymaganiaRouter = require('./wymagania/wymaganiaRouter')
 const historyRouter = require('./history/historyRouter')
+const projectsRouter = require('./projects/projectsRouter')
 
+router.use(express.json())
 
 router.use('/', indexRouter)
 router.use('/news', newsRouter)
@@ -19,6 +21,18 @@ router.use('/docs', docsRouter)
 router.use('/kadra', kadraRouter)
 router.use('/wymagania', wymaganiaRouter)
 router.use('/history', historyRouter)
+router.use('/projects', projectsRouter)
+
+router.use('/download', (req, res) => {
+    const file = __dirname + "/../public/" + req.body.file;
+
+    console.log(req.body);
+
+    res.set({
+        'Location': "URL://"
+    });
+    res.download(file);
+})
 
 
 module.exports = router;
